68d77f22cacc6406551cddfa4fd64c950a743292
SHREE BALAJI L <87368974+Shrel@users.noreply.github.com>
2023-07-09 22:58:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // const itemSchema = import("mongo_utils.js");
+// const mongoClient = require('mongodb').MongoClient;
 
 app.set('view engine','ejs');
 
@@ -10,39 +11,64 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
-
+const url = 'mongodb://127.0.0.1:27017/todolistDB';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Perform database operations...
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 const itemSchema = {
     task:{ type: String }
 };
 
-const Item = mongoose.model("Item",itemSchema);
+const Item = new mongoose.model("Item",itemSchema);
 
 const itemName1 = new Item({task:"welcome"});
 const itemName2 = new Item({task:"how is it?"});
 const itemName3 = new Item({task:"click + to add tasks"});
 const defaultItems = [itemName1,itemName2,itemName3];
+// defaultItems.save();//for saving one element
+// Item.insertMany([itemName1,itemName2,itemName3]);//for inserting multiple elements
 
-Item.create(itemName1)
+console.log("item saved");
 
-app.get('/',(req,res)=>{
-    // Item.find({},(err,Items_inserted)=>{
-    //     if(err)console.log(err);
-    //     else if(Items_inserted.length === 0){
-    //         Item.insertMany(defaultItems,(err)=>{
-    //             if(err)console.log(err);
-    //             else console.log("default items are inserted");
-    //         });
-    //     }
-    // res.render('list',{pro:"Progress",itemsList:Items_inserted});
-    // });
-    res.render('list',{pro:"Progress",itemsList:defaultItems});
+app.get('/', (req, res) => {
+  let display_items;
+  
+  Item.find()
+    .then((ItemsInDB) => {
+      display_items = ItemsInDB;
+        
+      // display_items.forEach(element => {
+      //   console.log(element.task);
+      // });
+  
+      // Pass the retrieved data to the view for rendering
+      res.render('list', { pro: "Progress", itemsList: display_items });
+    })
+    .catch((error) => {
+      console.error(error);
+      // Handle any errors that occur during the find() operation
+    });
 });
 
 app.post('/',(req,res)=>{
-    item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
+    let item = req.body.newItem;
+    // items.push(item);
+    // if(item != null){
+    const newItem = new Item({task:item});
+    newItem.save().then(()=>{
+        console.log("new item is inserted....")
+        res.redirect('/');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    // console.log(item + " " + typeof(item));
+    if(item === null)res.redirect('/');
 })
 const port = process.env.PORT || 3000;
 app.listen(port,()=>console.log("server connected..."+ port))
